58f466cf4ea9e6c926cde500d3747769d8136620
Kate Stretton <katestretton@Kates-MacBook-Air.local>
2022-07-14 09:54:33 +1200

    Update db function

--- server/db/bookView.js
+++ server/db/bookView.js
@@ -1,7 +1,24 @@
 const connection = require('./connection')
 
 function getBookDetails(id, db = connection) {
-  return db('books').where({ id }).select().first()
+  return db('books')
+    .where({ id })
+    .select(
+      'id',
+      'title',
+      'author',
+      'genre',
+      'publishing_details as publishingDetails',
+      'edition',
+      'isbn',
+      'summary',
+      'condition',
+      'image',
+      'user_id as userId',
+      'status',
+      'rating'
+    )
+    .first()
 }
 
 module.exports = { getBookDetails }
