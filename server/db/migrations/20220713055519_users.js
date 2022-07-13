4b31fed5b7ea97cc28d49b09a40498322960542d
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-07-13 18:09:57 +1200

    Add user table migration

--- server/db/migrations/20220713055519_users.js
+++ server/db/migrations/20220713055519_users.js
@@ -2,14 +2,21 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
-
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (t) => {
+    t.increments('id').primary()
+    t.text('name')
+    t.text('location')
+    t.text('favourite_genre')
+    t.integer('trading_tokens')
+    t.text('email')
+    t.text('image')
+  })
+}
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('users')
+}
