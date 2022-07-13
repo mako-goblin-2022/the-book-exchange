16d2623ca1323506078f7576eebd7a4487bcd368
Mandy Hale <mandyhale@Mandys-MacBook-Air.local>
2022-07-13 19:36:35 +1200

    test Footer component

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -4,8 +4,8 @@ function Footer () {
   return ( 
     <footer>
       <p>Share the books &#10084;</p>
-      <p>Got an idea to help us improve The Book Exchange and make the world a better place through easy-peasy bookswapping? Let us know by contacting us at <u>hello@thebookexchange.co.nz</u>. 
-      <span>&copy;The Book Exchange</span> </p>
+      <p>Got an idea to help us improve The Book Exchange and make the world a better place through easy-peasy bookswapping? Let us know by contacting us at <u>hello@thebookexchange.co.nz</u>. </p>
+      <span>&copy;The Book Exchange</span> 
     </footer>
    )
 }
