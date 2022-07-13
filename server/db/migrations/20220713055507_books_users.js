b2e8bb895546832daf2363d4a69448ef2cc91120
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-07-13 18:12:29 +1200

    Add books_users table migration

--- server/db/migrations/20220713055507_books_users.js
+++ server/db/migrations/20220713055507_books_users.js
@@ -2,14 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('books_users', (table) => {
+    table.integer('books_id').references('books.id')
+    table.integer('user_id').references('users.id')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('books_users')
+}
