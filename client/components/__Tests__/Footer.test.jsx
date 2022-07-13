13ae2924c494eb5e563ad857526864e35cf1fa3a
Mandy Hale <mandyhale@Mandys-MacBook-Air.local>
2022-07-13 19:37:01 +1200

    clear old code

--- client/components/__Tests__/Footer.test.jsx
+++ client/components/__Tests__/Footer.test.jsx
@@ -12,11 +12,8 @@ describe('<Footer/>', () => {
   it('renders a <p> tag that includes "The Book Exchange"', () => {
     render(<Footer />)
     expect.assertions(2)
-    // expect(screen.getAllByText(/The Book Exchange/)).toBeInTheDocument();
     const p = screen.getAllByText(/The Book Exchange/)
     console.log(p[0])
-    // const footer = screen.getByRole('footer')
-    // expect(footer).toContainHTML('p')
     expect(p[0]).not.toBeNull()
     expect(p[0]).toHaveTextContent('The Book Exchange')
     
