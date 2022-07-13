f82893ff2bf359ceee6e4e2acd473176bcfc8185
tim-greenslade <80554755+treegroves@users.noreply.github.com>
2022-07-13 19:38:25 +1200

    Add books seed data

--- server/db/seeds/books_users.js
+++ server/db/seeds/books_users.js
@@ -1,11 +1,8 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
- */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
+exports.seed = function (knex) {
+  return knex('users')
+    .del()
+    .then(function () {
+      return knex('users').insert([
     {books_id}
     {id: 1, colName: 'rowValue1'},
     {id: 2, colName: 'rowValue2'},
