0e636e00f1acc4d83eeba9b6b0f9b1468b9d78e8
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-14 16:02:11 +1200

    Add home api and test fix

--- client/apis/home.js
+++ client/apis/home.js
@@ -3,7 +3,6 @@ const homeApi = '/api/v1/home/'
 
 export function getBooksData() {
   return request.get(homeApi).then((res) => {
-    console.log(res.body)
-    return res.body
+    return res.body.books
   })
 }
