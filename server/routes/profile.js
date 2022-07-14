7e09d9085dfbc64d3827e153380ede143d57a79c
victoria-marchant <103162303+victoria-marchant@users.noreply.github.com>
2022-07-15 10:46:37 +1200

    add getbookby userid fx

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -14,4 +14,14 @@ router.get('/:id', (req, res) => {
     })
 })
 
+//GET /api/v1/profile/:id/books
+router.get('/:id/books', (req, res) => {
+  const user_id = req.params.id
+  db.getBooksByUserId(user_id)
+    .then((data) => res.json(data))
+    .catch(() => {
+      res.status(500).send('server error')
+    })
+})
+
 module.exports = router
