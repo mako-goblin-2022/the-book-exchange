d1f6bddbc01a8bf96bd3e8d4162644ca6295fe3a
Mandycodes <96666993+mandyh101@users.noreply.github.com>
2022-07-15 10:22:09 +1200

    write thunky action

--- client/actions/home.js
+++ client/actions/home.js
@@ -1,4 +1,4 @@
-import { getBooks } from '../apis/home'
+import { getBooksData } from '../apis/home'
 
 export const SET_BOOKS = 'SET_BOOKS'
 export const SET_BOOKS_ERROR = 'SET_BOOKS_ERROR'
@@ -14,5 +14,18 @@ export function setBooksError(errorMessage) {
   return {
     type: SET_BOOKS_ERROR,
     payload: errorMessage,
+  }
+}
+
+export function fetchBooks() {
+  return (dispatch) => {
+    return getBooksData()
+      .then((books) => {
+        dispatch(setBooks(books))
+        return null
+      })
+      .catch((err) => {
+        dispatch(setBooksError(err.message))
+      })
   }
 }
