ebe1c8612b56bba920f58ccd37f7c76de4f525f3
kate-stretton <81283807+kate-stretton@users.noreply.github.com>
2022-07-14 20:09:21 +1200

    Basic CSS for bookView

--- client/components/BookView.jsx
+++ client/components/BookView.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import {useParams, Link} from 'react-router-dom'
+import styles from '../styles/BookView.module.scss'
 
 import {fetchBook} from '../actions/bookView'
 
@@ -15,18 +16,21 @@ export default function BookView() {
 
   return (
     <>
-      <div className = 'book-view'>
-        <img src={book.image} alt='book cover'/>
-          <ul>
-            <li>Title: {book.title}</li>
-            <li>Author: {book.author}</li>
-            <li>Genre: {book.genre}</li>
-            <li>Publishing details: {book.publishing_details}</li>
-            <li>Edition: {book.edition}</li>
-            <li>ISBN: {book.isbn}</li>
-            <li>Summary: {book.summary}</li>
-            <li>Condition: {book.condition}</li>
-          </ul>
+      <div className = {styles.bookView}>
+        <div className = {styles.imgContainer}>
+        <img className = {styles.img} src={book.image} alt='book cover'/>
+        </div>
+          <div className = {styles.bookDetails}>
+            <h3>{book.title}</h3>
+            <p>{book.author}</p>
+            <p>Genre: {book.genre}</p>
+            <p>Publishing details: {book.publishing_details}</p>
+            <p>Edition: {book.edition}</p>
+            <p>ISBN: {book.isbn}</p>
+            <p>Summary: {book.summary}</p>
+            <p>Condition: {book.condition}</p>
+          </div>
+          
       </div>
       <div className = 'back-button'>
         <Link to={'/'}>
