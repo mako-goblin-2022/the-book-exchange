36b5a76a8876c100170759e70bbcc081e325a5c9
Mandy Hale <mandyhale@Mandys-Air.hub>
2022-07-15 15:46:45 +1200

    add style to home component

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchBooks } from '../actions/home'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   const books = useSelector((state) => state.home)
@@ -15,10 +16,10 @@ export default function Home() {
   return (
     <>
       <h1>Browse books!</h1>
-      <div>
+      <div className={styles.home}>
         {books.content &&
           books.content.map((book) => (
-            <img src={book.image} key={book.id} alt="Book" />
+            <img className="book-cover" src={book.image} key={book.id} alt="Book" />
           ))}
       </div>
     </>
